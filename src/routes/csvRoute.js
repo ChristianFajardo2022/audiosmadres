69de180f905dced436db513dcbc5a27ee960dd1a
jeffbaut13 <jeferssonabv@gmail.com>
2024-05-09 17:10:03 -0500

    creacion fecha y hora de ordenes

--- src/routes/csvRoute.js
+++ src/routes/csvRoute.js
@@ -1,27 +1,37 @@
 import express from "express";
-import { parse } from "json2csv";
 import { db } from "../config/firebaseAdminConfig.js";
+import { createObjectCsvStringifier } from "csv-writer";
 
 const csvRoutes = express.Router();
 
 csvRoutes.get("/export-users-csv", async (req, res) => {
   try {
     const snapshot = await db.collection("usuarios").get();
-    const users = snapshot.docs.map((doc) => doc.data());
+    const users = snapshot.docs.map((doc) => {
+      const userData = doc.data();
+      // Convertir la fecha de Firestore a una cadena legible
+      if (userData.createdAt && userData.createdAt.toDate) {
+        userData.createdAt = userData.createdAt.toDate().toLocaleString(); // O utiliza cualquier otro método de formateo de fecha
+      }
+      return userData;
+    });
 
-    // Especifica los campos que quieres incluir en el CSV
-    const fields = [
-      "firstname",
-      "email",
-      "customer_id",
-      "order_id",
-      "trx_status",
-      "audioRef",
-      "createdAt",
-    ];
-    const csv = parse(users, { fields });
+    // Crear el archivo CSV manualmente
+    const csvStringifier = createObjectCsvStringifier({
+      header: [
+        { id: "firstname", title: "Firstname" },
+        { id: "email", title: "Email" },
+        { id: "customer_id", title: "Customer ID" },
+        { id: "order_id", title: "Order ID" },
+        { id: "trx_status", title: "Transaction Status" },
+        { id: "audioRef", title: "Audio Reference" },
+        { id: "createdAt", title: "Created At" }, // Agregar el campo de fecha
+      ],
+    });
+    const csv =
+      csvStringifier.getHeaderString() + csvStringifier.stringifyRecords(users);
 
-    // Configura los headers para descargar el archivo
+    // Configurar los headers para descargar el archivo
     res.header("Content-Type", "text/csv");
     res.attachment("usuarios.csv");
     res.send(csv);
