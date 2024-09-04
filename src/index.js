a8b5890a03cce4273f528bc89cd628aa7ab79b7d
Christian Camilo Fajardo Hernandez <chfajard@gmail.com>
2024-09-04 09:31:07 -0500

    port

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import alcarritoRoute from "./routes/alcarritoRoute.js";
 import getUserRoute from "./routes/getUserRoute.js";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
